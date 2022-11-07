eed85e7c2f6045992d983b7186171e8c2c2d3033
meskyA <mesky2015@gmail.com>
2022-11-07 15:37:22 -0800

    adjusted contact js form to justify left

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,67 +34,6 @@ function ContactForm() {
     }
   }
 
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//   }
-
-//   return (
-//     <section className="container">
-//       <h2 data-testid="h1tag" className="top-title">
-//         Contact Form
-//       </h2>
-//       <hr></hr>
-//       <form class="justify-content-center" id="contact-form">
-//         <div class="mt-5">
-//           <label htmlFor="name">Name:</label>
-//           <input
-//             class="form-control"
-//             type="text"
-//             name="name"
-//             defaultValue={name}
-//             onBlur={handleChange}
-//           />
-//         </div>
-//         <div class="mt-5">
-//           <label htmlFor="email">Email</label>
-//           <input
-//             class="form-control"
-//             type="email"
-//             name="email"
-//             defaultValue={email}
-//             onBlur={handleChange}
-//           />
-//         </div>
-//         <div class="mt-5">
-//           <label htmlFor="message">Message</label>
-//           <textarea
-//             class="form-control"
-//             name="message"
-//             defaultValue={message}
-//             onBlur={handleChange}
-//             rows="7"
-//           />
-//         </div>
-//         {errorMessage && (
-//           <div>
-//             <p className="error-text">{errorMessage}</p>
-//           </div>
-//         )}
-
-//         <div class="mt-5 mb-5">
-//           <button
-//             data-testid="button"
-//             class="btn btn-outline-dark "
-//             type="submit"
-//             onSubmit={handleSubmit}
-//           >
-//             Submit
-//           </button>
-//         </div>
-//       </form>
-//     </section>
-//   );
-// }
 function handleSubmit(e) {
   e.preventDefault();
 }
@@ -103,7 +42,7 @@ return (
 <section className="container">
   <h2 data-testid='h1tag' className="top-title">Contact Form</h2>
   <hr></hr>
-  <form class="" id="contact-form">
+  <form class="justify-content-center" id="contact-form">
       <div class="mt-5" >
           <label htmlFor="name">Name:</label>
           <input class="form-control" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
@@ -113,8 +52,8 @@ return (
           <input class="form-control" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
       </div>
       <div class="mt-5" >
-          <label htmlFor="message">Message:</label>
-          <textarea class="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+          <label htmlFor="message">Message</label>
+          <textarea class="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="6" />
       </div> 
       {errorMessage && (
       <div>
