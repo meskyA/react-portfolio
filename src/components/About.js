7e5552e93ba0d6fafe0e904b39626e0190a193e8
meskyA <mesky2015@gmail.com>
2023-02-21 21:50:06 -0800

    changes profile pic

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
    
       <p className='content is-medium'>Full Stack Developer</p>
       <hr/>
-      <img class="mb-5" id="avatar" src="/react-portfolio/Assets/profilepic.jpg" alt="Meskerem 'Mesky' Assefa" />
+      <img class="mb-5" id="avatar" src="/react-portfolio/Assets/profile.jpg" alt="Meskerem 'Mesky' Assefa" />
       <p className='content'>
       Full Stack Web Developer graduated from a 6-months bootcamp at UC Berkeley Extension. Currently living in Oakland, CA and excited to transmit the new technologies I learned to create pleasant looking and responsive websites with user-friendliness in mind. Passionate about  adapting life experience of communication, cultural competence and leadership skills with this new journey and adventure of coding. Love traveling and appreciate the beauty of diversity in our world.
  
